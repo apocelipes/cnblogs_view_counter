8b7b4d978f1133b70389212aa53d1aa8127d5717
apocelipes <seve3r@outlook.com>
2021-03-17 14:43:29 +0900

    fix fetch blog posts

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/chromedp/cdproto/cdp"
@@ -34,15 +35,16 @@ func calcViewer(data <-chan PostInfo, ret chan<- uint64) {
 	ret <- sum
 }
 
-func getNextPageURL(url *string, currentPageIndex *int, nodes []*cdp.Node) chromedp.ActionFunc {
+func getNextPageURL(url *string, currentPageIndex *int) chromedp.ActionFunc {
 	return func(ctx context.Context) error {
+		nodes := make([]*cdp.Node, 0)
 		*url = ""
 		if *currentPageIndex == 1 {
 			var ok bool
 			// timeout means there's no next-page button
 			timeoutCtx, timeoutCancel := context.WithTimeout(ctx, 2*time.Second)
-			defer timeoutCancel()
 			err := chromedp.AttributeValue("#nav_next_page a", "href", url, &ok, chromedp.ByQuery).Do(timeoutCtx)
+			timeoutCancel()
 			if err != nil {
 				return err
 			}
@@ -63,6 +65,96 @@ func getNextPageURL(url *string, currentPageIndex *int, nodes []*cdp.Node) chrom
 			*url = node.AttributeValue("href")
 		}
 		*currentPageIndex += 1
+		return nil
+	}
+}
+
+var postTimePattern = regexp.MustCompile(`posted @ (\d{4}-\d{2}-\d{2}\s\d{2}:\d{2})`)
+var countPattern = regexp.MustCompile(`阅读\(([0-9]+)\)`)
+
+var wg sync.WaitGroup
+
+func parseClassDay(nodes []*cdp.Node, data chan<- PostInfo) {
+	for _, post := range nodes {
+		titleNode := searchNodeByClass(post.Children, "postTitle")
+		// .postTitle > a > span > TextNode
+		title := titleNode.Children[0].Children[0].Children[0].NodeValue
+		title = strings.TrimSpace(title)
+		descNode := searchNodeByClass(post.Children, "postDesc")
+		date, count := getPostMetaData(descNode)
+		data <- PostInfo{
+			Title:     title,
+			Date:      date,
+			ViewCount: count,
+		}
+	}
+	wg.Done()
+}
+
+func parseClassPost(nodes []*cdp.Node, data chan<- PostInfo) {
+	for _, post := range nodes {
+		titleNode := post.Children[0]
+		// .postTitle > a > span > TextNode
+		title := titleNode.Children[0].Children[0].Children[0].NodeValue
+		title = strings.TrimSpace(title)
+		descNode := searchNodeByClass(post.Children, "postfoot")
+		date, count := getPostMetaData(descNode)
+		data <- PostInfo{
+			Title:     title,
+			Date:      date,
+			ViewCount: count,
+		}
+	}
+	wg.Done()
+}
+
+func getPostMetaData(descNode *cdp.Node) (time.Time, uint64) {
+	// node > text
+	timeStr := postTimePattern.FindStringSubmatch(descNode.Children[0].NodeValue)[1]
+	date, _ := time.Parse("2006-01-02 15:04", timeStr)
+	// node > .post-view-count
+	viewCountNode := searchNodeByClass(descNode.Children, "post-view-count")
+	countStr := countPattern.FindStringSubmatch(viewCountNode.Children[0].NodeValue)[1]
+	count, _ := strconv.ParseUint(countStr, 10, 64)
+	return date, count
+}
+
+type PostType uint8
+
+const (
+	PostDay PostType = iota
+	PostPost
+)
+
+func getAllPosts(data chan<- PostInfo) chromedp.ActionFunc {
+	return func(ctx context.Context) error {
+		const timeLimit = 5 * time.Second
+		nodes := make([]*cdp.Node, 0, 6)
+		blogPostType := PostDay
+		timeoutCtx, timeoutCancel := context.WithTimeout(ctx, timeLimit)
+		err := chromedp.Nodes(".day", &nodes, chromedp.ByQueryAll).Do(timeoutCtx)
+		timeoutCancel()
+		if errors.Is(err, context.DeadlineExceeded) {
+			timeoutCtx, timeoutCancel := context.WithTimeout(ctx, timeLimit)
+			err := chromedp.Nodes(".post", &nodes, chromedp.ByQueryAll).Do(timeoutCtx)
+			timeoutCancel()
+			if err != nil {
+				return err
+			}
+			blogPostType = PostPost
+		}
+		if len(nodes) == 0 {
+			return errors.New("no post")
+		}
+
+		wg.Add(1)
+		switch blogPostType {
+		case PostDay:
+			go parseClassDay(nodes, data)
+		case PostPost:
+			go parseClassPost(nodes, data)
+		}
+
 		return nil
 	}
 }
@@ -102,38 +194,15 @@ func main() {
 	go calcViewer(data, resChan)
 
 	url := fmt.Sprintf("https://www.cnblogs.com/%s/", *blogUserName)
-	var nodes []*cdp.Node
 	pageCounter := 1
-	postTimePattern := regexp.MustCompile(`posted @ (\d{4}-\d{2}-\d{2}\s\d{2}:\d{2})`)
-	countPattern := regexp.MustCompile(`阅读\(([0-9]+)\)`)
 	for url != "" {
 		//TODO: progressbar
 		fmt.Println("page:", pageCounter)
 		err := chromedp.Run(ctx,
 			chromedp.Navigate(url),
-			chromedp.WaitReady(`.day *`, chromedp.ByQueryAll),
-			chromedp.Nodes(".day", &nodes, chromedp.ByQueryAll),
-			chromedp.ActionFunc(func(ctx context.Context) error {
-				for _, post := range nodes {
-					titleNode := searchNodeByClass(post.Children, "postTitle")
-					// .postTitle > a > span > TextNode
-					title := titleNode.Children[0].Children[0].Children[0].NodeValue
-					title = strings.TrimSpace(title)
-					descNode := searchNodeByClass(post.Children, "postDesc")
-					timeStr := postTimePattern.FindStringSubmatch(descNode.Children[0].NodeValue)[1]
-					date, _ := time.Parse("2006-01-02 15:04", timeStr)
-					viewCountNode := searchNodeByClass(descNode.Children, "post-view-count")
-					countStr := countPattern.FindStringSubmatch(viewCountNode.Children[0].NodeValue)[1]
-					count, _ := strconv.ParseUint(countStr, 10, 64)
-					data <- PostInfo{
-						Title:     title,
-						Date:      date,
-						ViewCount: count,
-					}
-				}
-				return nil
-			}),
-			getNextPageURL(&url, &pageCounter, nodes),
+			chromedp.WaitReady(`.day *, .post *`, chromedp.ByQueryAll),
+			getAllPosts(data),
+			getNextPageURL(&url, &pageCounter),
 		)
 		if err != nil {
 			log.Fatal(err)
@@ -141,5 +210,6 @@ func main() {
 		time.Sleep(time.Duration(rand.Intn(3)+1) * time.Second)
 	}
 	close(data)
+	wg.Wait()
 	fmt.Printf("User: %s\ttotal view count: %v\n", *blogUserName, <-resChan)
 }
