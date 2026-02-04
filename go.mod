40715ce10053503911897a73058439000f4d41a3
apocelipes <seve3r@outlook.com>
2026-02-04 19:12:30 +0800

    chore: upgrade

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/apocelipes/cnblogs_view_counter
 
-go 1.25.3
+go 1.25.6
 
 require (
 	github.com/chromedp/cdproto v0.0.0-20250803210736-d308e07a266d
@@ -13,5 +13,5 @@ require (
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.4.0 // indirect
-	golang.org/x/sys v0.37.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
 )
--- main.go
+++ main.go
@@ -149,7 +149,7 @@ func main() {
 		chromedp.Headless,
 		chromedp.DisableGPU,
 		chromedp.Flag("disable-extensions", true),
-		chromedp.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"),
+		chromedp.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36"),
 	)
 	defer cc()
 	// create chrome instance
